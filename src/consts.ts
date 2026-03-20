f9fccd07e7a533baef778960ca610dd517ecdcf0
plbraindrain-maker <plbraindrain@gmail.com>
2026-03-20 00:18:05 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Panoramic Omics Sciences Center';
-export const SITE_DESCRIPTION = 'Education and Service in the Omics Sciences.';
+export const SITE_TITLE = 'PanorOmics | The Panoramic Omics Sciences Center';
+export const SITE_DESCRIPTION = 'Advancing precision medicine through multi-omics integration, epigenomics, and bioinformatics data science.';
 
